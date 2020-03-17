79048a90301aa3ee733b981eadb6f9b15029c0ee
zingp <605156398@qq.com>
2020-03-17 21:00:41 +0800

    git ignore

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python run.py --model "TextCNN"
+python run.py --model "TextRNN"
+python run.py --model "FastText"
+python run.py --model "TextRCNN"
+python run.py --model "TextRNN_Att"
+python run.py --model "DPCNN"
+python run.py --model "Transformer"
